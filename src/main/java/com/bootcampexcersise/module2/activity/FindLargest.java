bd03bd98dad8875f308d498245d8ce60e7840aad
vmambetova <vitamambetova@gmail.com>
2022-07-25 16:54:41 +0300

    Find largest

---
+++ src/main/java/com/bootcampexcersise/module2/activity/FindLargest.java
@@ -0,0 +1,16 @@
+package com.bootcampexcersise.module2.activity;
+
+public class FindLargest {
+
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 4;
+
+        if (a>b){
+            System.out.println("A is greater than b");
+        } else {
+            System.out.println("B is greater than a");
+        }
+    }
+}

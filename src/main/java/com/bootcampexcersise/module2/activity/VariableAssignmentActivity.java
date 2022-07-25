a9d0d25d8dc0b6f120d2d2abcb228836ab9c3c24
vmambetova <vitamambetova@gmail.com>
2022-07-25 14:47:16 +0300

    Updated int value
    Assignment corrected

--- src/main/java/com/bootcampexcersise/module2/activity/VariableAssignmentActivity.java
+++ src/main/java/com/bootcampexcersise/module2/activity/VariableAssignmentActivity.java
@@ -3,8 +3,8 @@ package com.bootcampexcersise.module2.activity;
 public class VariableAssignmentActivity {
     public static void main(String[] args) {
 
-    int a;
-    a = 5;
+    int a = 10;
+    a = 12;
         System.out.println("Value a = " + a);
 }
 }

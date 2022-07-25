14d9fb68112d5c8dec5d16c79e8ed57a596706ce
vmambetova <vitamambetova@gmail.com>
2022-07-25 14:42:21 +0300

    Variable Assignment added

---
+++ src/main/java/com/bootcampexcersise/module2/activity/VariableAssignmentActivity.java
@@ -0,0 +1,10 @@
+package com.bootcampexcersise.module2.activity;
+
+public class VariableAssignmentActivity {
+    public static void main(String[] args) {
+
+    int a;
+    a = 5;
+        System.out.println("Value a = " + a);
+}
+}

543e94b3233cc8051620e01956db505f6a7b4c14
vmambetova <vitamambetova@gmail.com>
2022-07-25 17:23:28 +0300

    NumToWords

---
+++ src/main/java/com/bootcampexcersise/module2/activity/NumToWords.java
@@ -0,0 +1,41 @@
+package com.bootcampexcersise.module2.activity;
+
+public class NumToWords {
+    public static void main(String[] args) {
+        int x = 5;
+        switch (x){
+            case 1:
+                System.out.println("Value of x is not 1");
+                break;
+            case 2:
+                System.out.println("Value of x is not 2");
+                break;
+            case 3:
+            System.out.println("Value of x is not 3");
+            break;
+            case 4:
+                System.out.println("Value of x is not 4");
+                break;
+            case 5:
+                System.out.println("Value of x is 5");
+            break;
+            case 6:
+                System.out.println("Value of x is not 6");
+                break;
+            case 7:
+                System.out.println("Value of x is not 7");
+            break;
+            case 8:
+                System.out.println("Value of x is not 8");
+                break;
+            case 9:
+                System.out.println("Value of x is not 9");
+                break;
+            case 10:
+                System.out.println("Value of x is not 10");
+                break;
+            default:
+                System.out.println("Value of x is null");
+        }
+    }
+}

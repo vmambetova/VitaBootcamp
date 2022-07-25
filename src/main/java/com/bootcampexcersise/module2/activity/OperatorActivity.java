9d4a13f4eca313aff02f8357bca0eb33d971b012
vmambetova <vitamambetova@gmail.com>
2022-07-25 16:25:00 +0300

    Operators activity

---
+++ src/main/java/com/bootcampexcersise/module2/activity/OperatorActivity.java
@@ -0,0 +1,11 @@
+package com.bootcampexcersise.module2.activity;
+
+public class OperatorActivity {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int sum = a + b;
+        System.out.println(sum);
+
+    }
+}

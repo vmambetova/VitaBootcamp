74009a47ba336dc09b9518d44b8d4bd14028b991
vmambetova <vitamambetova@gmail.com>
2022-07-25 17:32:43 +0300

    Correct Switch activity

--- src/main/java/com/bootcampexcersise/module2/activity/NumToWords.java
+++ src/main/java/com/bootcampexcersise/module2/activity/NumToWords.java
@@ -5,34 +5,34 @@ public class NumToWords {
         int x = 5;
         switch (x){
             case 1:
-                System.out.println("Value of x is not 1");
+                System.out.println("One");
                 break;
             case 2:
-                System.out.println("Value of x is not 2");
+                System.out.println("Two");
                 break;
             case 3:
-            System.out.println("Value of x is not 3");
+            System.out.println("Three");
             break;
             case 4:
-                System.out.println("Value of x is not 4");
+                System.out.println("Four");
                 break;
             case 5:
-                System.out.println("Value of x is 5");
+                System.out.println("Five");
             break;
             case 6:
-                System.out.println("Value of x is not 6");
+                System.out.println("Six");
                 break;
             case 7:
-                System.out.println("Value of x is not 7");
+                System.out.println("Seven");
             break;
             case 8:
-                System.out.println("Value of x is not 8");
+                System.out.println("Eight");
                 break;
             case 9:
-                System.out.println("Value of x is not 9");
+                System.out.println("Nine");
                 break;
             case 10:
-                System.out.println("Value of x is not 10");
+                System.out.println("Ten");
                 break;
             default:
                 System.out.println("Value of x is null");

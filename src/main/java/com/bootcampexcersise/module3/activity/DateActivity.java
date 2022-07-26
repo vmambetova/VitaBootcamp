50f889f648c26b0e134c745a6a4d7c5a68025664
vmambetova <vitamambetova@gmail.com>
2022-07-26 14:18:16 +0300

    Create DateActivity.java

---
+++ src/main/java/com/bootcampexcersise/module3/activity/DateActivity.java
@@ -0,0 +1,11 @@
+package com.bootcampexcersise.module3.activity;
+import java.util.Date;
+public class DateActivity {
+
+    public static void main(String[] args) {
+        Date todayDate = new Date();
+        System.out.println(todayDate);
+    }
+
+
+}

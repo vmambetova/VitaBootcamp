e4c0e8a0e0dfcaa6de9deb77722f8a6546cad86e
vmambetova <vitamambetova@gmail.com>
2022-07-26 09:21:10 +0300

    AddWholeNr

---
+++ src/main/java/com/bootcampexcersise/module2/activity/AddWholeNum.java
@@ -0,0 +1,11 @@
+package com.bootcampexcersise.module2.activity;
+
+public class AddWholeNum {
+    public static void main(String[] args) {
+        int sum = 50;
+        for (int i = 51; i <=100; i++){
+            sum = sum+1;
+        }
+        System.out.println("Sum of numbers 50-100 is: " + sum);
+    }
+}

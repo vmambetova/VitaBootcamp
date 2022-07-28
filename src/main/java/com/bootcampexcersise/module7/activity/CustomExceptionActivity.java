694236aa2a684ca6decf4c5c392ba0f4f71ea4f1
vmambetova <vitamambetova@gmail.com>
2022-07-28 12:39:12 +0300

    Custom Exceptions

---
+++ src/main/java/com/bootcampexcersise/module7/activity/CustomExceptionActivity.java
@@ -0,0 +1,13 @@
+package com.bootcampexcersise.module7.activity;
+
+//Needs to be completed
+//1 - extend this class from Exception class
+public class CustomExceptionActivity extends Exception {
+        public String getMessage(){
+            return "You are not allowed to use the program";
+        }
+    //2 - Override getMessage() and provide the following message
+    // "You are not allowed to use this program"
+
+
+}

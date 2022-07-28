009fa3c81a6efee8642ed095141526407cdbe84c
vmambetova <vitamambetova@gmail.com>
2022-07-28 10:23:38 +0300

    ExceptionActivity

--- src/main/java/com/bootcampexcersise/module7/activity/ArithmeticExceptionActivity.java
+++ src/main/java/com/bootcampexcersise/module7/activity/ArithmeticExceptionActivity.java
@@ -3,13 +3,10 @@ package com.bootcampexcersise.module7.activity;
 public class ArithmeticExceptionActivity {
 
     public static void main(String[] args) {
-      /*  int num1 = 10;
-        int num2 = 0;
-        int num3 = 2;*/
+
         ArithmeticExceptionActivity arithmeticExceptionActivity = new ArithmeticExceptionActivity();
         arithmeticExceptionActivity.catchMe(10, 0);
         arithmeticExceptionActivity.catchMe(10, 2);
-        System.out.println(arithmeticExceptionActivity);
     }
 
     void catchMe(int num1, int num2) {
@@ -21,8 +18,6 @@ public class ArithmeticExceptionActivity {
         }finally{
             System.out.println("Thank you for using this program.");
         }
-        int result = num1 / num2;
-        System.out.println("The result is :" + result);
 
 
     }

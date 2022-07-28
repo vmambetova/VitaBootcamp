47d5eb89507861a188cc8aec6440e2153a076845
vmambetova <vitamambetova@gmail.com>
2022-07-28 13:02:44 +0300

    CustomExceptionActivity

--- src/main/java/com/bootcampexcersise/module7/activity/CustomExceptionActivityTest.java
+++ src/main/java/com/bootcampexcersise/module7/activity/CustomExceptionActivityTest.java
@@ -2,21 +2,20 @@ package com.bootcampexcersise.module7.activity;
 
 //This program accepts a user name and checks user's validity
 //The user name is checked against an array of names.
-
 public class CustomExceptionActivityTest {
     public static void main(String[] args) throws CustomExceptionActivity {
         CustomExceptionActivityTest user = new CustomExceptionActivityTest();
-
-
+        user.validateUser("John");
+        user.validateUser("Vita"); // just to see what happens if name is not in the array
         //1 - Type main method and call validateUser() from it.
         //Call this method with different names to test it
-
+    }
         void validateUser (String name) throws CustomExceptionActivity{
             String[] validUsers = {"John", "Mike", "Shanti", "Stacie"};
 //flag stores 1 if a match is found else it should remain 0
             int flag = 0;
             for (int i = 0; i < 4; i++) {
-                if validUsers[i] == name) {
+                if (validUsers[i] == name) {
                     flag = 1;
                 }
             }
@@ -27,13 +26,5 @@ public class CustomExceptionActivityTest {
             }
 
         }
-//2 - Write code to check if parameter name contains a value which is found in validUsers array and change flag's value accordingly
 
-            }
-//3 - check if flag is zero, throw CustomExceptionActivity Exception.
-//You may also have to declare this exception in the method call using throws.
-
-
-//4 - else if flag is one, print a message "Welcome to Payroll program".
-
-        }
+    }

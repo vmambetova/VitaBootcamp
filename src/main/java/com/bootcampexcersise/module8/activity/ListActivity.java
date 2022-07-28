ce202a7b1a2ec7d2299d81b328fc2a2844d13127
vmambetova <vitamambetova@gmail.com>
2022-07-28 13:59:38 +0300

    ListActivity

--- src/main/java/com/bootcampexcersise/module8/activity/ListActivity.java
+++ src/main/java/com/bootcampexcersise/module8/activity/ListActivity.java
@@ -4,6 +4,7 @@ package com.bootcampexcersise.module8.activity;
 
 import java.awt.print.PrinterGraphics;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class ListActivity {
@@ -11,18 +12,33 @@ public class ListActivity {
     public static void main(String[] args) {
         //TODO: 1 - Type code to create a list of names. Use ArrayList.
         List list = new ArrayList();
-        list.add("c");
-        list.add("d");
+        list.add("Ross");
+        list.add("Rachel");
+        list.add("Monica");
+        list.add("Joey");
+        list.add("Chandler");
+        list.add("Phoebe");
 
         //TODO: 2 - Call print method to print the list passed as its parameter.
         ListActivity print = new ListActivity();
-       print.print(list);
+        print.print(list);
     }
 
     void print(List list) {
         System.out.println(list);
         //TODO: 3 - Type code to print this list
         //Notice the order in which elements get printed.
+        Iterator<Object> nameOfIterator = list.iterator();
+        System.out.println("*** Work of iterator ***");
+        while (nameOfIterator.hasNext()) {
+            System.out.println(nameOfIterator.next());
+        }
 
-    }
-}
+        System.out.println("*** Work of for loop ***");
+        for (Object nameOfMyObject : list) {
+            System.out.println(nameOfMyObject.toString());
+        }
+
+
+    }}
+

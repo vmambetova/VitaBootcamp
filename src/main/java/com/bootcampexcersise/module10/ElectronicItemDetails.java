cbcbc49481156f41a45f3c281b7532227025a8b8
vmambetova <vitamambetova@gmail.com>
2022-07-29 16:19:18 +0300

    JavaFinalActivity

--- src/main/java/com/bootcampexcersise/module10/ElectronicItemDetails.java
+++ src/main/java/com/bootcampexcersise/module10/ElectronicItemDetails.java
@@ -14,9 +14,7 @@ public class ElectronicItemDetails {
         itemHashMap.put("Laptop", 1240.6);
         ElectronicItemDetails obj = new ElectronicItemDetails();
         System.out.println("The price of the TV is " + obj.returnPriceOfItem(itemHashMap, "TV"));
-        System.out.println("The price of the refrigerator is " + obj.returnPriceOfItem(itemHashMap, "Refrigerator"));
-        System.out.println("The price of the washing machine is " + obj.returnPriceOfItem(itemHashMap, "Washing Machine"));
-        System.out.println("The price of the laptop is " + obj.returnPriceOfItem(itemHashMap, "Laptop"));
+        
     }
 
     public Double returnPriceOfItem(Map<String, Double> map, String name){
